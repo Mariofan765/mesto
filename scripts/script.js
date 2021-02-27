025e42eb6b076997c29940ea5774bb982ccdf234
Michail Zhivoglyad <mariofan7652003@yandex.ru>
2021-02-27 10:55:46 +0300

    second-commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,29 @@
 let popup = document.querySelector('.popup');
 let activePopup = document.querySelector('.profile__edit');
+let profileName = document.querySelector('.profile__name');
+let profileStatus = document.querySelector('.profile__status');
+let popupName = document.querySelector('.popup__input_name');
+let popupStatus = document.querySelector('.popup__input_status');
+let profileEdit = document.querySelector('.popup__form');
+let disablePopup = document.querySelector('.popup__close');
 
 function popupAct() {
   popup.classList.add('popup_openned'); 
   popupStatus.value = profileStatus.textContent;
   popupName.value = profileName.textContent;
 }
-activePopup.addEventListener('click', popupAct);
- 
-
-let disablePopup = document.querySelector('.popup__close');
 
 function popupDis() {
   popup.classList.remove('popup_openned');
 }
-disablePopup.addEventListener('click', popupDis);
 
-
-let profileName = document.querySelector('.profile__name');
-let profileStatus = document.querySelector('.profile__status');
-let popupName = document.querySelector('.popup__name');
-let popupStatus = document.querySelector('.popup__status');
-let profileEdit = document.querySelector('.popup__submit');
-
-function profileData() {
+function profileData(evt) {
+  evt.preventDefault();
   profileName.textContent = popupName.value;
   profileStatus.textContent = popupStatus.value;
 }
-profileEdit.addEventListener('click', popupDis);
-profileEdit.addEventListener('click', profileData);
+
+disablePopup.addEventListener('click', popupDis);
+activePopup.addEventListener('click', popupAct);
+profileEdit.addEventListener('submit', popupDis);
+profileEdit.addEventListener('submit', profileData);
